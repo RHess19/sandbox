e4bfbf49798c87242d3c3b23731e533a1049fdd5
Ryan Hessler <51040604+RHess19@users.noreply.github.com>
2025-05-12 10:47:12 -0600

    Add examples of arrays, hashes, .remainder(), and .divmod()

--- Ruby/basic-data-types/script.rb
+++ Ruby/basic-data-types/script.rb
@@ -6,8 +6,15 @@ puts 2*2
 puts 10/5
 puts 2**2
 puts 3**4
-puts 8%2
-puts 10%4
+
+# Remainder and modulo
+# modulo returns + when the second operand is +, - when the second operand is -
+# remainder returns + when the first operand is +, - when the first operand is -
+puts 8%2 # 0
+puts 10%4 # 2
+
+puts 16.remainder(5) # 1
+puts 16.divmod(5) # [3, 1]
 
 
 # Integers and floats
@@ -79,6 +86,7 @@ nil.to_s
 
 # Symbols - stored in memory only once, even if a duplicate value symbol already exists. With strings, even if an existing string with that value exists, it occupies a separate memory spot
 # Create symbol with : followed by the symbol name
+# Symbol names don't have to be valid variable names if placed within quotes. Ex. :"this is a valid symbol"
 
 puts "string" == "string" # true
 puts "string".object_id == "string".object_id # false
@@ -86,3 +94,20 @@ puts :symbol.object_id == :symbol.object_id # true
 
 
 # Booleans - true, false, nil
+
+# test for nil
+puts "Hello".nil? # false
+puts false == nil # false
+
+
+# Arrays and hashes
+test_array = [1, 2, 3, 4, 5]
+puts test_array[1] # 2
+
+# hashes are also called dictionaries
+test_hash = {:dog => 'barks', :cat => 'meows', :pig => 'oinks'}
+puts test_hash[:cat]
+
+# new hash syntax - only works when using symbols as keys
+test_hash2 = {dog: 'barks', cat: 'meows', pig: 'oinks'}
+puts test_hash2[:dog] # must still put a : before the key when accessing items
